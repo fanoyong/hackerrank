10aaa70c556a2b3eb1d5b1e9600d203d7591b187
Hwanho Yong <hwanho.yong@sta.samsung.com>
2015-01-02 16:06:54 -0500

    alternating characters

--- src/algorithms/warmup/alternating_char.c
+++ src/algorithms/warmup/alternating_char.c
@@ -3,13 +3,27 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main ()
-{
-  int c;
-  puts ("Enter text. Include a dot ('.') in a sentence to exit:");
-  do {
-    c=getchar();
-    putchar (c);
-  } while (c != '.');
-  return 0;
+int main () {
+    int T;
+    int i;
+    int c, oldc;
+    int count;
+    scanf("%d", &T);
+    getc(stdin);
+    for (i=0; i<T; i++) {
+        oldc = -99;
+        count = 0;
+        while(1) {
+            c=getc(stdin);
+            if (c=='\n' || c=='\r' || c==EOF) {
+                break;
+            }
+            if (c==oldc) {
+                count++;
+            }
+            oldc = c;
+        }
+        printf("%d\n", count);
+    }
+    return 0;
 }
