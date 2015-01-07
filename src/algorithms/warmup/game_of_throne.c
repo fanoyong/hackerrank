cf5dc327a5a1b6a5ffde13d4ff44978b08d25d19
Hwanho Yong <hwanho.yong@sta.samsung.com>
2015-01-07 14:38:14 -0500

    game of throne

--- src/algorithms/warmup/game_of_throne.c
+++ src/algorithms/warmup/game_of_throne.c
@@ -5,10 +5,28 @@
  
 findPalind(char *arr)
 {
-     
+    int count[50];
     int flag = 0;
     // Find the required answer here. Print Yes or No at the end of this function depending on your inspection of the string
-    if (flag==0)
+    int i, j;
+    int len = strlen(arr);
+    for (i=0; i<50; i++)
+    {
+        count[i] = 0;
+    }
+    for (i=0; i<len; i++)
+    {
+        count[arr[i] - 'a']++;
+    }
+    for(i=0;i<50;i++)
+    {
+        if (count[i] % 2 == 1)
+        {
+            flag++;
+        }
+    }
+
+    if (flag==0 || flag == 1)
         printf("YES\n");
     else
         printf("NO\n");
