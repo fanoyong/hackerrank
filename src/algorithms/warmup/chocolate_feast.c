c01b147976163c9d5dd8a03c0cc4c216050ef997
Hwanho Yong <hwanho.yong@sta.samsung.com>
2015-01-12 14:38:54 -0500

    bug fix for chocolate feast

--- src/algorithms/warmup/chocolate_feast.c
+++ src/algorithms/warmup/chocolate_feast.c
@@ -7,14 +7,18 @@ int main() {
 
     int t;
     long n, c, m;
+    long count=0, wrapper=0;
     scanf("%d", &t);
     while ( t-- )
     {
         scanf("%ld%ld%ld",&n,&c,&m);
-        long count = (long) (n/c);
-        long bonus = ((long) (n/c)) / m;
-        long bonus_extra = ((((long) (n/c)) % m) + bonus) / m;
-        long answer = count + bonus + bonus_extra;
+        count = (long) n/c;
+        wrapper = count;
+        while (wrapper / m > 0) {
+            count += (wrapper/m);
+            wrapper = (wrapper/m) + (wrapper%m);
+        }
+        long answer = count;
         /** Write the code to compute the answer here. **/
         printf("%ld\n", answer);
     }
