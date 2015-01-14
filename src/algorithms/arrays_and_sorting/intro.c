3d0ad6ca47c57b7921bf696744d670cdf5625a72
Hwanho Yong <hwanho.yong@sta.samsung.com>
2015-01-14 16:50:38 -0500

    intro

--- src/algorithms/arrays_and_sorting/intro.c
+++ src/algorithms/arrays_and_sorting/intro.c
@@ -5,7 +5,17 @@
 #include <stdlib.h>
 
 int main() {
-
+    int V, n;
+    int i,t;
+    scanf("%d %d", &V, &n);
+    for (i=0; i<n; i++) {
+        scanf("%d", &t);
+        if(V==t) {
+            printf("%d\n",i);
+            return 0;
+        }
+    }
+    printf("\n");
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     return 0;
 }
