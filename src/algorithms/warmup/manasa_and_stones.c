d34bd008e42ac72db742d5dd63ab9f82e45f1563
Hwanho Yong <fanoyong@gmail.com>
2015-01-14 00:32:50 -0500

    manasa and stones

--- src/algorithms/warmup/manasa_and_stones.c
+++ src/algorithms/warmup/manasa_and_stones.c
@@ -4,7 +4,25 @@
 #include <stdlib.h>
 
 int main() {
-
+    int i,j;
+    int T, n, a, b, t;
+    scanf("%d",&T);
+    while (T--) {
+        scanf("%d %d %d", &n, &a, &b);
+        if (a>b) {
+            t = a;
+            a = b;
+            b = t;
+        }
+        if (a==b) {
+            printf ("%d", a*(n-1));
+        } else {
+        for (i=0; i<n; i++) {
+            printf("%d ", a*(n-i-1)+b*i);
+        }
+        }
+        printf("\n");
+    }
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     return 0;
 }
