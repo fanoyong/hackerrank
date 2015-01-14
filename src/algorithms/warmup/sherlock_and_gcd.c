b47c481695c93ad578aad7786bf00c016c4e4f7b
Hwanho Yong <fanoyong@gmail.com>
2015-01-14 01:58:29 -0500

    sherlock and gcd

--- src/algorithms/warmup/sherlock_and_gcd.c
+++ src/algorithms/warmup/sherlock_and_gcd.c
@@ -3,6 +3,31 @@
 #include <math.h>
 #include <stdlib.h>
 
+int gcd (int n, int A[]) {
+    int i,j;
+    int max, min;
+    while(1) {
+       max = 0;
+        min = 100001;
+        for (i=0; i<n; i++) {
+            if (A[i] < min) {
+                min = A[i];
+                j = i;
+            }
+            if (A[i] > max) {
+               max = A[i];
+            }
+        }
+        if (max == min) {
+            return min;
+        }
+        for (i=0;i<n;i++) {
+            if (i!=j && A[i] != A[j]) A[i] -= min;
+        }
+    }
+    return 1;
+}
+
 int main() {
     int i,j,t;
     int T, n, A[100];
@@ -10,20 +35,12 @@ int main() {
     while (T--) {
         scanf("%d",&n);
         for (i=0; i<n; i++) {
-            scanf("%d", &t);            
-            A[i] = t; 
+            scanf("%d", &t);
+            A[i] = t;
         }
-        t = 0;
-        while (A[t] <= 1) {
-            t++;        
-        }
-        for (i=1; i<n; i++) {
-            if (A[i]%A[t] != 0) {
-                printf("YES\n");
-                break;
-            }
-        }
-        if (i==n) {
+        if (gcd(n, A) == 1) {
+            printf("YES\n");
+        } else {
             printf("NO\n");
         }
     }
