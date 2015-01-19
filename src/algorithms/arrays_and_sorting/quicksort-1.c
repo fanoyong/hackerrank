00ebb30b75aef938b3e480b5beb16bf5d2e0f1a0
Hwanho Yong <hwanho.yong@sta.samsung.com>
2015-01-19 18:10:00 -0500

    skeleton for quicksort 2

--- src/algorithms/arrays_and_sorting/quicksort-1.c
+++ src/algorithms/arrays_and_sorting/quicksort-1.c
@@ -4,42 +4,47 @@
 #include <stdlib.h>
 #include <assert.h>
 
-void printAr(int ar_size, int * ar) {
+int count_l, count_r;
+
+void printSolution (int pivot, int * ar_l, int * ar_r) {
     int i;
-    for(i=0;i<ar_size;i++) {
-        printf("%d ",ar[i]);
+    for (i=0; i<count_l; i++) {
+        printf("%d ", ar_l[i]);
     }
-    printf("\n");
+    printf("%d ", pivot);
+    for (i=0; i<count_r; i++) {
+        printf("%d ", ar_r[i]);
+    }
 }
 
-void partition(int ar_size, int *  ar, int lo, int hi) {
-    int pivotIndex = lo;
+void partition(int ar_size, int *  ar, int * ar_l, int * ar_r) {
+    int pivotIndex = 0;
     int pivotValue = ar[pivotIndex];
-    int ar2[ar_size];
     int i;
-    int count=0;
+    count_l=0;
     for (i=0; i<ar_size; i++) {
         if(ar[i] < pivotValue) {
-            ar2[count++] = ar[i];
+            ar_l[count_l++] = ar[i];
         }
     }
+    count_r=0;
     for (i=0; i<ar_size; i++) {
-        if(ar[i] >= pivotValue) {
-            ar2[count++] = ar[i];
+        if(ar[i] > pivotValue) {
+            ar_r[count_r++] = ar[i];
         }
     }
-    printAr(ar_size, ar2);
 }
 
 int main(void) {
     int _ar_size;
     scanf("%d", &_ar_size);
     int _ar[_ar_size], _ar_i;
+    int _ar_l[_ar_size], _ar_r[_ar_size];
     for(_ar_i = 0; _ar_i < _ar_size; _ar_i++) {
         scanf("%d", &_ar[_ar_i]);
     }
-    partition(_ar_size, _ar, 0, _ar_size-1);
-
+    partition(_ar_size, _ar, _ar_l, _ar_r);
+    printSolution(_ar[0], _ar_l, _ar_r);
     return 0;
 }
 
