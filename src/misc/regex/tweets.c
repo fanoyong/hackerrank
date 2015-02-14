6a2bbd5cba530b3256b44a2abc91aa023cc634fb
Hwanho Yong <fanoyong@gmail.com>
2015-02-14 17:39:41 -0500

    tweet

--- src/misc/regex/tweets.c
+++ src/misc/regex/tweets.c
@@ -7,13 +7,16 @@
 #define FALSE 0
 #define DEBUG 0
 
-int isChar(char);
-int isNum(char);
+const int TWEET_LEN = 10;
+const char TWEET_KEYWORD[11] = "hackerrank";
+
 int isValid(char *);
+int isTweetMatch(char *, int);
 
 int main () {
     int N;
-    char input[200];
+    char input[2000];
+    int count;
 //    freopen("test.input", "r", stdin);
     setbuf(stdout, NULL);
     scanf("%d",&N);
@@ -21,54 +24,70 @@ int main () {
 #if DEBUG
     printf("N:%d\n", N);
 #endif
+    count = 0;
     while(N-- > 0) {
-        fgets(input, 200, stdin);
-        if(isValid(input)) {
-            printf("YES\n");
-        } else {
-            printf("NO\n");
-        }
+        fgets(input, sizeof(input), stdin);
 #if DEBUG
-        printf("N:%d\n", N);
+        printf("main input: %s\n", input);
 #endif
+        if(isValid(input)) {
+            count++;
+        }
     }
+    printf("%d\n", count);
     return 0;
 }
 
 int isValid(char * input) {
-    int ret =  strlen(input) == 11 &&
-        isChar(input[0]) &&
-        isChar(input[1]) &&
-        isChar(input[2]) &&
-        isChar(input[3]) &&
-        isChar(input[4]) &&
-        isNum(input[5]) &&
-        isNum(input[6]) &&
-        isNum(input[7]) &&
-        isNum(input[8]) &&
-        isChar(input[9]);
+    int ret = FALSE;
+    int len = strlen(input);
+    int i;
+#if DEBUG
+    printf("isValid: input:%s len:%d\n", input, len);
+#endif
+    for (i=0; i<len; i++) {
+        if ((input[i] == 'h' || input[i] == 'H') && (len-i) >= TWEET_LEN) {
+            if (isTweetMatch(input, i)) {
+#if DEBUG
+                printf("Match!\n");
+#endif
+               return TRUE;
+            } else {
+#if DEBUG
+                printf("Not match\n");
+#endif
+                i += TWEET_LEN;
+            }
+        }
+    }
 #if DEBUG
     printf("isValid: %d\n", ret);
 #endif
     return ret;
 }
 
-int isChar(char c) {
-    int ret;
-    ret = c >= 'A' && c <= 'Z';
+int isTweetMatch (char * input, int index) {
+    int i;
+    for (i=0; i< TWEET_LEN; i++) {
 #if DEBUG
-    printf("isChar c:%c\n", c);
-    printf("isChar: %d\n", ret);
+        printf("Character %c=%d\n", input[i+index], input[i+index]);
+        printf("a = %c=%d\n", 'a', 'a');
 #endif
-    return ret;
-}
-
-int isNum(char c) {
-   int ret = c >= '0' && c <= '9';
+        if (input[i+index] < 'a') {
 #if DEBUG
-    printf("isNum c:%c\n", c);
-    printf("isNum: %d\n", ret);
+            printf("Capital character: %c\n", input[i+index]);
 #endif
-    return ret;
+            input[i+index] += ('a' - 'A');
+#if DEBUG
+            printf("input is %c:%d\n", input[i+index], input[i+index]);
+#endif
+        }
+        if(input[i+index] != TWEET_KEYWORD[i]) {
+#if DEBUG
+            printf("Return false\n");
+#endif
+            return FALSE;
+        }
+    }
+    return TRUE;
 }
-
