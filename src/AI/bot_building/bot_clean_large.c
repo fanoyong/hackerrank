13e41ee3e6df4d4bc042061ed1d9cbeafcba6570
Hwanho Yong <hwanho.yong@samsung.com>
2015-05-05 16:46:59 -0400

    more optimized algorithm

--- src/AI/bot_building/bot_clean_large.c
+++ src/AI/bot_building/bot_clean_large.c
@@ -1,96 +1,157 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #define DEBUG 0
 
+typedef struct struct_coordinate {
+    int x;
+    int y;
+} coordinate;
+
 int w,h;
+char board[50][50];
+coordinate destination;
+
+int valid_x_y(int x, int y);
+int traverse(int depth, int max_depth, int i, int j, coordinate * destination);
+void next_move(int posr, int posc);
+void find_target(int posr, int posc, coordinate * destination);
+void print_path(coordinate * current, coordinate * destination);
+void print_board();
+
+int main(void) {
+    int pos[2], dim[2], i;
+    scanf("%d", &pos[0]);
+    scanf("%d", &pos[1]);
+    scanf("%d", &dim[0]);
+    scanf("%d", &dim[1]);
+    h = dim[0];
+    w = dim[1];
+    for(i=0; i<h; i++) {
+        scanf("%s", board[i]);
+    }
+    next_move(pos[0], pos[1]);
+    return 0;
+}
+
 
 int valid_x_y(int x, int y) {
     return (x>=0) && (x<h) && (y>=0) && (y<w);
 }
 
-int traverse(int depth, int max_depth, int i, int j, char board[50][50]) {
-#if DEBUG
-    printf("traverse depth:%d i:%d j:%d\n", depth, i, j);
-#endif
+int traverse(int depth, int max_depth, int i, int j, coordinate * destination) {
     if (depth > max_depth) {
-#if DEBUG
-        printf("Error 1\n");
-#endif
         return 0;
     }
-    if (depth > max_depth) {
-#if DEBUG
-        printf("Error 2\n");
-#endif
-        return 0;
-    } 
-    if (!valid_x_y(i,j)) {
-#if DEBUG
-        printf("Error 3\n");
-#endif
-        return 0;
-    }
-    
     if (board[i][j] == 'd') {
-#if DEBUG
-        printf("got it\n");
-#endif
+        destination->x = i;
+        destination->y = j;
         return 1;
     }
-    if (traverse(depth+1, max_depth, i-1,j,board)) {
-        return 2;
-    } else if (traverse(depth+1, max_depth, i, j+1, board)) {
-        return 3;
-    } else if (traverse(depth+1, max_depth, i+1, j, board)) {
-        return 4;
-    } else if (traverse(depth+1, max_depth, i, j-1, board)) {
-        return 5;
+    if (i < h/2 && j < w/2) {
+        if (valid_x_y(i-1,j) && traverse(depth+1, max_depth, i-1, j, destination)) {
+            return 1;
+        } else if (valid_x_y(i,j-1) && traverse(depth+1, max_depth, i, j-1, destination)) {
+            return 1;
+        } else if (valid_x_y(i,j+1) && traverse(depth+1, max_depth, i, j+1, destination)) {
+            return 1;
+        } else if (valid_x_y(i+1,j) && traverse(depth+1, max_depth, i+1, j, destination)) {
+            return 1;
+        }
+    } else if (i < h/2 && j >= w/2) {
+        if (valid_x_y(i-1,j) && traverse(depth+1, max_depth, i-1, j, destination)) {
+            return 1;
+        } else if (valid_x_y(i,j+1) && traverse(depth+1, max_depth, i, j+1, destination)) {
+            return 1;
+        } else if (valid_x_y(i,j-1) && traverse(depth+1, max_depth, i, j-1, destination)) {
+            return 1;
+        } else if (valid_x_y(i+1,j) && traverse(depth+1, max_depth, i+1, j, destination)) {
+            return 1;
+        }
+    } else if (i >= h/2 && j >= w/2) {
+        if (valid_x_y(i+1,j) && traverse(depth+1, max_depth, i+1, j, destination)) {
+            return 1;
+        } else if (valid_x_y(i,j+1) && traverse(depth+1, max_depth, i, j+1, destination)) {
+            return 1;
+        } else if (valid_x_y(i-1,j) && traverse(depth+1, max_depth, i-1, j, destination)) {
+            return 1;
+        } else if (valid_x_y(i,j-1) && traverse(depth+1, max_depth, i, j-1, destination)) {
+            return 1;
+        }
+    } else {
+        if (valid_x_y(i+1,j) && traverse(depth+1, max_depth, i+1, j, destination)) {
+            return 1;
+        } else if (valid_x_y(i,j-1) && traverse(depth+1, max_depth, i, j-1, destination)) {
+            return 1;
+        } else if (valid_x_y(i,j+1) && traverse(depth+1, max_depth, i, j+1, destination)) {
+            return 1;
+        } else if (valid_x_y(i-1,j) && traverse(depth+1, max_depth, i-1, j, destination)) {
+            return 1;
+        }
     }
-#if DEBUG
-    printf("Found nothing\n");
-#endif
+    
     return 0;
 }
 
-void next_move(int posr, int posc, char board[50][50]) {
+void next_move(int posr, int posc) {
     int i;
     int ret;
      if (board[posr][posc] == 'd') {
         printf("CLEAN\n");
         return;
     }
-    for (i=1; i<=w+h; i++) {
-        ret = traverse(1, i+1, posr, posc, board);
-        switch (ret) {
-            case 2:
-                printf("UP\n");
-                return;
-            case 3:
-                printf("RIGHT\n");            
-                return;
-            case 4:
-                printf("DOWN\n");            
-                return;
-            case 5:
-                printf("LEFT\n");            
-                return;
+    coordinate *current, *destination;
+    current = malloc(sizeof(coordinate));
+    current->x = posr;
+    current->y = posc;
+    destination = malloc(sizeof(coordinate));
+    destination->x = -1;
+    destination->y = -1;
+
+    find_target(posr, posc, destination);
+    print_path(current, destination);
+
+    if (NULL != destination) {
+        free(destination);
+    }
+    if (NULL != current) {
+        free(current);
+    }
+}
+
+void find_target(int x, int y, coordinate * destination){
+    int i;
+    int ret;
+    for(i=1; i<=w+h; i++) {
+        if(traverse(1, i+1, x, y, destination)) {
+            return;
         }
     }
 }
 
-int main(void) {
-    int pos[2], dim[2], i;
-    char board[50][50];
-    scanf("%d", &pos[0]);
-    scanf("%d", &pos[1]);
-    scanf("%d", &dim[0]);
-    scanf("%d", &dim[1]);
-    for(i=0; i<dim[0]; i++) {
-        scanf("%s[^\\n]%*c", board[i]);
+void print_path(coordinate * current, coordinate * destination){
+    if(valid_x_y(destination->x, destination->y) && valid_x_y(current->x, current->y)) {
+        if(destination->x < current->x) {
+            printf("UP\n");
+        } else if (destination->x > current->x) {
+            printf("DOWN\n");
+        } else {
+            if(destination->y > current->y) {
+                printf("RIGHT\n");
+            } else if (destination->y < current->y) {
+                printf("LEFT\n");
+            } else {
+                printf("CLEAN\n");
+            }
+        }
     }
-    h = dim[0];
-    w = dim[1];
-    next_move(pos[0], pos[1], board);
-    return 0;
 }
 
+void print_board() {
+    int i;
+    printf("\n");
+    for(i=0; i<h; i++) {
+        printf("%s\n", board[i]);
+    }
+    printf("\n");
+}
--- src/AI/bot_building/bot_clean_partial.c
+++ src/AI/bot_building/bot_clean_partial.c
@@ -1,76 +1,177 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
 #define DEBUG 0
 
+typedef struct struct_coordinate {
+    int x;
+    int y;
+} coordinate;
+
 int w,h;
-int max;
+char board[5][5];
+
+int distance(coordinate * origin, coordinate * destination);
+int valid_x_y(int x, int y);
+int traverse(int depth, int max_depth, int i, int j, coordinate * destination);
+void next_move(int posr, int posc);
+void find_target(coordinate * current, coordinate * destination);
+void print_path(coordinate * current, coordinate * destination);
+void print_board();
+
+int main(void) {
+    int i;
+    int pos[2];
+    w = 5;
+    h = 5;
+    srand(time(NULL));
+    scanf("%d", &pos[0]);
+    scanf("%d", &pos[1]);
+    for(i=0; i<h; i++) {
+        scanf("%s", board[i]);
+    }
+    next_move(pos[0], pos[1]);
+    return 0;
+}
+
+int distance(coordinate * origin, coordinate * destination) {
+    return abs(origin->x - destination->x) + abs(origin->y - destination->y);
+}
 
 int valid_x_y(int x, int y) {
     return (x>=0) && (x<h) && (y>=0) && (y<w);
 }
 
-int traverse(int depth, int max_depth, int i, int j, char board[50][50]) {
-#if DEBUG
-    printf("traverse depth:%d i:%d j:%d\n", depth, i, j);
-#endif
-    if (depth > max_depth || depth > max || !valid_x_y(i,j)) {
+int traverse(int depth, int max_depth, int i, int j, coordinate * destination) {
+    if (depth > max_depth) {
         return 0;
     }
     if (board[i][j] == 'd') {
+        destination->x = i;
+        destination->y = j;
         return 1;
-    }
-    if (traverse(depth+1, max_depth, i-1,j,board)) {
-        return 2;
-    } else if (traverse(depth+1, max_depth, i, j+1, board)) {
-        return 3;
-    } else if (traverse(depth+1, max_depth, i+1, j, board)) {
-        return 4;
-    } else if (traverse(depth+1, max_depth, i, j-1, board)) {
-        return 5;
     }
     return 0;
 }
 
-void next_move(int posr, int posc, char board[50][50]) {
-    int i;
-    int ret;
+void next_move(int posr, int posc) {
      if (board[posr][posc] == 'd') {
         printf("CLEAN\n");
         return;
     }
-    for (i=1; i<=max; i++) {
-        ret = traverse(1, i+1, posr, posc, board);
-        switch (ret) {
+    coordinate *current, *destination;
+    current = malloc(sizeof(coordinate));
+    current->x = posr;
+    current->y = posc;
+    destination = malloc(sizeof(coordinate));
+    destination->x = -1;
+    destination->y = -1;
+
+    find_target(current, destination);
+    print_path(current, destination);
+
+    if (NULL != destination) {
+        free(destination);
+    }
+    if (NULL != current) {
+        free(current);
+    }
+}
+
+void find_target(coordinate * current, coordinate * destination){
+    int i,j;
+    int dist = 10000000;
+    int new_dist;
+    coordinate *temp;
+    temp = malloc(sizeof(coordinate));
+    for(i=0; i<h; i++) {
+        for(j=0; j<w; j++) {
+            if(board[i][j] == 'd') {
+                temp->x = i;
+                temp->y = j;
+                new_dist = distance(current,temp);
+                if(new_dist < dist) {
+                    dist = new_dist;
+                    destination->x = i;
+                    destination->y = j;
+                }
+            }
+        }
+    }
+    free(temp);
+}
+
+void print_path(coordinate * current, coordinate * destination){
+    int direction, zone;
+    if(valid_x_y(destination->x, destination->y) && valid_x_y(current->x, current->y)) {
+        if(destination->x < current->x) {
+            printf("UP\n");
+        } else if (destination->x > current->x) {
+            printf("DOWN\n");
+        } else {
+            if(destination->y > current->y) {
+                printf("RIGHT\n");
+            } else if (destination->y < current->y) {
+                printf("LEFT\n");
+            } else {
+                printf("CLEAN\n");
+            }
+        }
+    } else {
+        direction = rand() % 2;
+        if (current->x <= (h/2)) {
+            if(current->y <= (w/2)) {
+                zone = 0;
+            } else {
+                zone = 1;
+            }
+        } else {
+            if(current->y <= (w/2)) {
+                zone = 3;
+            } else {
+                zone = 2;
+            }
+        }
+        switch(zone) {
+            case 0:
+                if(direction) {
+                    printf("DOWN\n");
+                } else {
+                    printf("RIGHT\n");
+                }
+                break;
+            case 1:
+                if(direction) {
+                    printf("DOWN\n");
+                } else {
+                    printf("LEFT\n");
+                }
+                break;
             case 2:
-                printf("UP\n");
-                return;
+                if(direction) {
+                    printf("UP\n");
+                } else {
+                    printf("LEFT\n");
+                }
+                break;
             case 3:
-                printf("RIGHT\n");            
-                return;
-            case 4:
-                printf("DOWN\n");            
-                return;
-            case 5:
-                printf("LEFT\n");            
-                return;
+                if(direction) {
+                    printf("UP\n");
+                } else {
+                    printf("RIGHT\n");
+                }
+                break;
         }
     }
 }
 
-int main(void) {
-    int pos[2], dim[2], i;
-    char board[50][50];
-    scanf("%d", &pos[0]);
-    scanf("%d", &pos[1]);
-    scanf("%d", &dim[0]);
-    scanf("%d", &dim[1]);
-    for(i=0; i<dim[0]; i++) {
-        scanf("%s[^\\n]%*c", board[i]);
+void print_board() {
+    int i;
+    printf("\n");
+    for(i=0; i<h; i++) {
+        printf("%s\n", board[i]);
     }
-    h = dim[0];
-    w = dim[1];
-    max = dim[0] + dim[1];
-    next_move(pos[0], pos[1], board);
-    return 0;
+    printf("\n");
 }
-
