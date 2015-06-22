e14fc8116d8eba3b2bc43b4bbb815b53a5cedd4c
Hwanho Yong <hwanho.yong@samsung.com>
2015-06-22 16:04:42 -0400

    compare/merge

---
+++ src/data_structure/linked_list/compare.java
@@ -0,0 +1,21 @@
+/*
+  Merge two sorted lists A and B as one linked list
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+Node* MergeLists(Node *headA, Node* headB)
+{
+    Node *head;
+    while (headA != NULL || headB != NULL)
+    {
+        if(headA->data > headB->data)
+        {
+        
+        }
+    }
+}
+
