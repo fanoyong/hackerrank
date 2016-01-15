bd79793ea34ecac29f04d7b2b0aa9138465e3780
Hwanho Yong <hwanho.yong@samsung.com>
2016-01-15 15:47:03 -0500

    Project Euler #1

--- src/Project_Euler/001_Multiples_of_3_and_5/code.cpp
+++ src/Project_Euler/001_Multiples_of_3_and_5/code.cpp
@@ -6,21 +6,24 @@
 using namespace std;
 
 void brute_force(int N);
+void calculate(long N);
 
 int main() {
-    int T,N;
+    int T;
+    long N;
     cin >> T;
     while (T--)
     {
         cin >> N;
-        brute_force(N);   
+        // brute_force(N);   
+        calculate(N);
     }
     return 0;
 }
 
-void brute_force(int N) {
-    int i;
-    int sum=0;
+void brute_force(long N) {
+    long i;
+    long sum=0;
     for(i=3; i<N; i+=3) {
         sum+=i;
     }
@@ -33,3 +36,12 @@ void brute_force(int N) {
     cout << sum << endl;
 }
 
+void calculate(long N) {
+    long num_of_3 = (long) (N-1)/3;
+    long num_of_5 = (long) (N-1)/5;
+    long num_of_15 = (long) (N-1)/15;
+    long sum_of_3 = 3 * (num_of_3 + 1) * num_of_3 / 2;
+    long sum_of_5 = 5 * (num_of_5 + 1) * num_of_5 / 2;
+    long sum_of_15 = 15 * (num_of_15 + 1) * num_of_15 / 2;
+    cout << sum_of_3 + sum_of_5 - sum_of_15 << endl;
+}
