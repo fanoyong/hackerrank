b7142901673c504a742d8a3c790a03bb963e558a
Hwanho Yong <hwanho.yong@samsung.com>
2016-03-22 11:18:57 -0400

    Morgan and String2

--- src/algorithms/strings/morgan_and_string2.cpp
+++ src/algorithms/strings/morgan_and_string2.cpp
@@ -9,21 +9,21 @@
 using namespace std;
 
 #define LL long long
-#define DEBUG 0
+#define DEBUG 1
 
 const LL LIMIT = 100001;
 
 void print (string a, LL i, string b, LL j, string c);
 
 int main() {
-    ifstream fin; 
-    fin.open("input.morgan3");
+    // ifstream fin; 
+    // fin.open("input.morgan3");
     string a, b, c;
     int T;
-    fin >> T;
+    cin >> T;
     while (T--) {
         //char a[LIMIT], b[LIMIT], c[LIMIT];
-        fin >> a >> b;
+        cin >> a >> b;
         const LL len_a = a.length() - 1;
         const LL len_b = b.length() - 1;
         char ca = '\0';
@@ -36,11 +36,11 @@ int main() {
         while (i <= len_a || j <= len_b) {
             #if DEBUG
                 cout << "i/j:" << i << "/" << j << endl;
+                print (a,i,b,j,c);
             #endif
             if (i > len_a) {
                 #if DEBUG
                     cout << "A reached end" << endl;
-                    print (a,i,b,j,c);
                 #endif
                 c.append(b.substr(j,len_b-j+1));
                 break;
@@ -50,7 +50,6 @@ int main() {
             if (j > len_b) {
                 #if DEBUG
                     cout << "B reached end" << endl;
-                    print (a,i,b,j,c);
                 #endif
                 c.append(a.substr(i,len_a-i+1));
                 break;
@@ -59,7 +58,6 @@ int main() {
             }
             #if DEBUG
                 cout << "CA/CB:" << ca << "/" << cb << endl;
-                print (a,i,b,j,c);
             #endif
             if (ca < cb) {
                 #if DEBUG
@@ -76,14 +74,26 @@ int main() {
             } else {
                 string ar = a.substr(i,len_a-i+1);
                 string br = b.substr(j,len_b-j+1);
+                #if DEBUG
+                   cout << "ar:" << ar << " br:" << br << endl;
+                #endif
                 int cp = ar.compare(br);
                 if (cp < 0) {
+                    #if DEBUG
+                        cout << "Case 3" << endl;
+                    #endif
                     c.push_back(ca);
                     i++;
                 } else if (cp > 0) {
+                    #if DEBUG
+                        cout << "Case 4" << endl;
+                    #endif
                     c.push_back(cb);
                     j++;
                 } else {
+                    #if DEBUG
+                        cout << "Case 5" << endl;
+                    #endif
                     c.push_back(cb);
                     j++;
                 }
@@ -97,8 +107,8 @@ int main() {
 void print (string a, LL i, string b, LL j, string c)
 {
     LL i2,j2;
-    cout << "A:" << a.substr(i, a.length()-1-i) << endl;
-    cout << "B:" << b.substr(j, b.length()-1-j) << endl;
+    cout << "A:" << a.substr(i, a.length()-i) << endl;
+    cout << "B:" << b.substr(j, b.length()-j) << endl;
     cout << "C:" << c << endl;
 }
 
