98b72d4053d9b97dea6ee9894d6f9d51045c1d77
Hwanho Yong <hwanho.yong@samsung.com>
2016-03-23 17:37:25 -0400

    Coin change

---
+++ src/algorithms/dynamic_programming/coin_change.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int N, M;
+    int C[50];
+    int count = 0;
+    cin >> N >> M;
+    for (int i = 0; i<M; i++) {
+        cin >> C[i];
+    }
+    cout << count << endl;
+    return 0;
+}
+
