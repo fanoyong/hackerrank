0556ca740c3020200a4c58a1a9702e6f3b6eeb44
Hwanho Yong <fanoyong@gmail.com>
2016-04-12 21:57:25 -0400

    Counting sort - full

--- src/algorithms/sorting/counting_sort_full.cpp
+++ src/algorithms/sorting/counting_sort_full.cpp
@@ -1,5 +1,6 @@
 #include <cmath>
 #include <cstdio>
+#include <cstring>
 #include <vector>
 #include <iostream>
 #include <algorithm>
@@ -9,23 +10,48 @@ int main() {
     int N;
     cin >>N;
     unsigned int DATA[1000001], COUNT[100];
-    unsigned int DATA22[1000001];
+    unsigned int DATA2[1000001];
     char** DATAS = (char **) malloc(sizeof(char*) * N);
     for(int i = 0; i<100; i++) {
-        COUNT[i] = N;
+        COUNT[i] = 0;
     }
-    for(int i = 0; i<N; i++) {
+    // Step 1 - Histogram
+    for(int i = 0; i<N/2; i++) {
         cin >> DATA[i];
-        string s;
+        COUNT[DATA[i]]++;
+        char s[11];
         cin >> s;
-        DATAS[i] =(char *) malloc(sizeof(char) * ((s.length()+1)));
-        s.copy(DATAS[i], s.length(),0);
-        for (int j = 0; j<DATA[i]; j++) {
-            COUNT[j]--;
-        }
+        s[0] = '-';
+        s[1] = '\0';
+        DATAS[i] =(char *) malloc(sizeof(char) * 2);
+        strcpy(DATAS[i], s);
     }
+    for(int i = N/2; i<N; i++) {
+        cin >> DATA[i];
+        COUNT[DATA[i]]++;
+        char s[11];
+        cin >> s;
+        DATAS[i] =(char *) malloc(sizeof(char) * (strlen(s)+1));
+        strcpy(DATAS[i], s);
+    }
+
+    // Step 2 - Calculate the starting index for each key
+    int total = 0;
     for (int i=0; i<100; i++) {
-        cout << COUNT[i] << " ";
+        int old_count = COUNT[i];
+        COUNT[i] = total;
+        total += old_count;
+    }
+
+    // Step 3 - Copy to output array
+    for (int i=0; i<N; i++) {
+        DATA2[COUNT[DATA[i]]] = i;
+        COUNT[DATA[i]]++;
+    }
+
+
+    for (int i=0; i<N; i++) {
+        cout << DATAS[DATA2[i]] << " ";
     }
     cout << endl;
     for (int i=0;i<N; i++) {
