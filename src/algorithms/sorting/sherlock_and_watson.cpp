7c75917e8529d4cf7a7346d95ffed2b5c2de4592
Hwanho Yong <hwanho.yong@samsung.com>
2016-06-01 15:50:03 -0400

    Shelock and Watson

--- src/algorithms/sorting/sherlock_and_watson.cpp
+++ src/algorithms/sorting/sherlock_and_watson.cpp
@@ -5,9 +5,23 @@
 #include <algorithm>
 using namespace std;
 
+#define LL long long
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-        return 0;
+    LL n, k, q, r;
+    cin >> n >> k >> q;
+    vector<LL> arr(n);
+    for (LL i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+    for(LL i=0; i<q; i++) {
+        cin >> r;
+        LL index = r-k;
+        while (index < 0) {
+            index += n;
         }
+        cout << arr[index%n] << endl;
+    }
+    return 0;
+}
 
