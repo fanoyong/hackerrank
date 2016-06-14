7f82488c49f3e329ec4b2333d39927834d3c5d84
Hwanho Yong <hwanho.yong@samsung.com>
2016-06-14 14:56:31 -0400

    Coin change

--- src/algorithms/dynamic_programming/coin_change.cpp
+++ src/algorithms/dynamic_programming/coin_change.cpp
@@ -4,24 +4,42 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
-#define DEBUG 1
+#define LL long long
+#define debug 1
+#define MOD 1000000007
 int N, M;
 int C[51];
+LL solution[51][251];
 
-int bottom_up();
+void generateAnswer();
 
 int main() {
-    int count = 0;
     cin >> N >> M;
-    for (int i=1; i<=M; i++) {
+    for (int i=0; i<M; i++) {
         cin >> C[i];
     }
-    int answer = bottom_up();
-    cout << answer << endl;
+    generateAnswer();
+    LL answer = solution[M][N] % MOD;
+    cout << (answer % MOD) << endl;
     return 0;
 }
 
-int bottom_up() {
-
+void generateAnswer() {
+    for (LL i=0; i<= M; i++) {
+        solution[i][0] = 1;
+    }
+    for (LL i=0; i<= N; i++) {
+        solution[0][i] = 0;
+    }
+    for (LL j=1; j<= N; j++) {
+        for (LL i=1; i<=M; i++) {
+            if (C[i-1] <= j) {
+                solution[i][j] = solution[i - 1][j] + solution[i][j - C[i-1]];
+                solution[i][j] %= MOD;
+            } else {
+                solution[i][j] = solution[i - 1][j];
+                solution[i][j] %= MOD;
+            }
+        }
+    }
 }
