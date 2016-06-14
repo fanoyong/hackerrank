6836f6fe134c9f75eb11d08670625670f466a12a
Hwanho Yong <hwanho.yong@samsung.com>
2016-06-14 14:58:38 -0400

    Coin change

--- src/algorithms/dynamic_programming/coin_change.cpp
+++ src/algorithms/dynamic_programming/coin_change.cpp
@@ -6,7 +6,6 @@
 using namespace std;
 #define LL long long
 #define debug 1
-#define MOD 1000000007
 int N, M;
 int C[51];
 LL solution[51][251];
@@ -19,8 +18,8 @@ int main() {
         cin >> C[i];
     }
     generateAnswer();
-    LL answer = solution[M][N] % MOD;
-    cout << (answer % MOD) << endl;
+    LL answer = solution[M][N];
+    cout << answer << endl;
     return 0;
 }
 
@@ -35,10 +34,8 @@ void generateAnswer() {
         for (LL i=1; i<=M; i++) {
             if (C[i-1] <= j) {
                 solution[i][j] = solution[i - 1][j] + solution[i][j - C[i-1]];
-                solution[i][j] %= MOD;
             } else {
                 solution[i][j] = solution[i - 1][j];
-                solution[i][j] %= MOD;
             }
         }
     }
