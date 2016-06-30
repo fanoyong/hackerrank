898dccf0d6f94400a0c262900c85bdb639d0b1b4
Hwanho Yong <hwanho.yong@samsung.com>
2016-06-30 15:09:06 -0400

    probability

---
+++ src/AI/probability_and_statistics/probability_puzzle_10.cpp
@@ -0,0 +1,6 @@
+Bill and Nina appear for an interview for two vacancies. The probability of Bill’s selection is 1/3 and that of Nina’s selection is 1/5. Find the probability that only one of them will be selected.
+
+Fill in the answer in the text box, in the form of an irreducible fraction A/B where A and B are two integers.
+
+e.g. Your answer should resemble something like:
+1/3*4/5 + 2/3*1/5 = 4+2/15 = 2/5
