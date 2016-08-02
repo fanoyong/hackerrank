de7efa95054e6ea508757f72d2408d20cdaa7a91
Hwanho Yong <hwanho.yong@samsung.com>
2016-08-02 14:43:16 -0400

    max draws

---
+++ src/mathematics/fundamental/max_draws.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int T;
+    int N;
+    cin >> T;
+    while (T--) {
+        cin >> N;
+        cout << N+1 << endl;
+    }
+    return 0;
+}
