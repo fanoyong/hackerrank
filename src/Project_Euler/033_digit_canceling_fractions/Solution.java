ae025de5b8e90632e1171436a70d0a27f7115e1c
Hwanho Yong <hwanho.yong@samsung.com>
2016-11-14 17:43:19 -0500

    Project Euler with Java - skeleton

---
+++ src/Project_Euler/033_digit_canceling_fractions/Solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+
+    private class Number implements Comparable<Number>{
+
+        // There to be HashMap of digit - count
+        
+
+        @Override
+        public int compareTo(Number o) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+    }
+
+}
+
