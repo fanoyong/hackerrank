2aa5966f1e80779f7a0317bc8f489091a98a8436
Hwanho Yong <hwanho.yong@samsung.com>
2016-11-15 17:41:43 -0500

    Now O(N^2) solution...

--- src/Project_Euler/039_integer_right_triangles/Solution.java
+++ src/Project_Euler/039_integer_right_triangles/Solution.java
@@ -1,64 +1,35 @@
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Solution {
 
-    private static ArrayList<Integer> answers = new ArrayList<Integer>();
+    private static HashMap<Long, Integer> answers = new HashMap<Long, Integer>();
 
     public static void main(String[] args) {
-        generateData();
+        getMaxPerimeter();
         System.out.println("data generated");
         Scanner scanner = new Scanner(System.in);
         int t = scanner.nextInt();
         while (t-- > 0) {
-            int n = scanner.nextInt();
-            System.out.println("data generated");
+            Long n = scanner.nextLong();
         }
         scanner.close();
     }
 
-    private static int iprev = 0;
-
-    private static void generateData() {
-        int cur = 0;
-        for (int i = 12; i < 5000; i++) {
-            int max = getMaxPerimeter(i);
-            if (max > cur) {
-                cur = max;
-                answers.add(cur);
-            }
-        }
-    }
-
-    // a+b+c <= n
-    // a^2 + b^2 = c^2
-    private static int getMaxPerimeter(int n) {
-        int maxp = n;
+    private static void getMaxPerimeter() {
         int maxcount = 0;
-        for (int p = iprev; p <= n; p++) {
+        for (long p = 2; p <= 10000; p++) {
             int count = 0;
-            for (int a = 1; a < p / 2; a++) {
-                int asq = a * a;
-                for (int b = 1; b <= a; b++) {
-                    int c = p - a - b;
-                    if (c > a + b) {
-                        continue;
-                    }
-                    int bsq = b * b;
-                    int csq = c * c;
-                    if (asq + bsq == csq) {
-                        count++;
-                    }
+            for (long a = 2; a < (p / 3); a++) {
+                if (p * (p - 2 * a) % (2 * (p - a)) == 0) {
+                    count++;
                 }
             }
             if (count > maxcount) {
-                iprev = p;
-                maxcount = count;
-                maxp = p;
+                answers.put(p, count);
             }
         }
-        return maxp;
     }
 }
 
