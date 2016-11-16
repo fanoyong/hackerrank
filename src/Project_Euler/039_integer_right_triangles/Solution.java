bab7b40608a9567a19ea7d9cdb3ea2d5e082ff0e
Hwanho Yong <hwanho.yong@samsung.com>
2016-11-16 16:36:08 -0500

    Project Euler #39

--- src/Project_Euler/039_integer_right_triangles/Solution.java
+++ src/Project_Euler/039_integer_right_triangles/Solution.java
@@ -1,35 +1,83 @@
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Solution {
-
-    private static HashMap<Long, Integer> answers = new HashMap<Long, Integer>();
+    private static final int MAX_P = 5000000;
+    private static final List<Integer> TRIPLET_LIST = new ArrayList<Integer>();
+    private static final Set<Integer> TRIPLET_SET = new HashSet<Integer>();
+    private static int[] SIEVE = new int[MAX_P + 1];
+    private static boolean[] ISPRIME = new boolean[MAX_P + 1];
 
     public static void main(String[] args) {
+        generateTriplets();
         getMaxPerimeter();
-        System.out.println("data generated");
         Scanner scanner = new Scanner(System.in);
         int t = scanner.nextInt();
         while (t-- > 0) {
-            Long n = scanner.nextLong();
+            int n = scanner.nextInt();
+            System.out.println(SIEVE[n]);
         }
         scanner.close();
     }
 
     private static void getMaxPerimeter() {
-        int maxcount = 0;
-        for (long p = 2; p <= 10000; p++) {
-            int count = 0;
-            for (long a = 2; a < (p / 3); a++) {
-                if (p * (p - 2 * a) % (2 * (p - a)) == 0) {
-                    count++;
-                }
+        TRIPLET_LIST.addAll(TRIPLET_SET);
+        Collections.sort(TRIPLET_LIST);
+        for (int i = 0; i <= MAX_P; i++) {
+            SIEVE[i] = 0;
+            ISPRIME[i] = true;
+        }
+        for (int p : TRIPLET_LIST) {
+            for (int i = p * 2; i <= MAX_P; i += p) {
+                ISPRIME[i] = false;
             }
-            if (count > maxcount) {
-                answers.put(p, count);
+        }
+        for (int p : TRIPLET_LIST) {
+            if (!ISPRIME[p]) {
+                continue;
+            }
+            for (int i = p; i <= MAX_P; i += p) {
+                SIEVE[i]++;
+            }
+        }
+        int cur = 0;
+        int curi = 0;
+        for (int i = 0; i <= MAX_P; i++) {
+            if (SIEVE[i] > cur) {
+                cur = SIEVE[i];
+                curi = i;
+            }
+            SIEVE[i] = curi;
+        }
+    }
+
+    private static void generateTriplets() {
+        for (int m = 3; m < 3000; m += 2) {
+            int msq = m * m;
+            for (int n = 1; n <= m; n += 2) {
+                if (n != 1 && m % n == 0) {
+                    continue;
+                }
+                int nsq = n * n;
+                int a = m * n;
+                int b = (msq - nsq) / 2;
+                int c = (msq + nsq) / 2;
+                int p = a + b + c;
+                if (p > MAX_P) {
+                    continue;
+                }
+                if (TRIPLET_SET.contains(p)) {
+                    continue;
+                }
+                TRIPLET_SET.add(p);
             }
         }
     }
+
 }
 
