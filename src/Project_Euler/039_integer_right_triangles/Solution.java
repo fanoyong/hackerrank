7753462242613f2a9de5a363e9d8acff62e14d6f
Hwanho Yong <hwanho.yong@samsung.com>
2016-11-30 17:21:21 -0500

    Project Euler #39 - revising logic

--- src/Project_Euler/039_integer_right_triangles/Solution.java
+++ src/Project_Euler/039_integer_right_triangles/Solution.java
@@ -1,63 +1,52 @@
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Solution {
     private static final int MAX_P = 5000000;
-    private static final List<Integer> TRIPLET_LIST = new ArrayList<Integer>();
-    private static final Set<Integer> TRIPLET_SET = new HashSet<Integer>();
     private static int[] SIEVE = new int[MAX_P + 1];
-    private static boolean[] ISPRIME = new boolean[MAX_P + 1];
+    private static int[] SIEVEI = new int[MAX_P + 1];
+    private static HashMap<Integer, Triple> TRIPLESET = new HashMap<Integer, Triple>();
+
+    private static class Triple implements Comparable<Triple> {
+        private int mA;
+        private int mB;
+        private int mC;
+        private int mP;
+
+        public Triple(int a, int b, int c) {
+            mA = a;
+            mB = b;
+            mC = c;
+            mP = a + b + c;
+        }
+
+        @Override
+        public int compareTo(Triple o) {
+            return Integer.compare(mP, o.mP);
+        }
+
+        @Override
+        public String toString() {
+            return mP + " " + mA + " " + mB + " " + mC;
+        }
+    }
 
     public static void main(String[] args) {
         generateTriplets();
-        getMaxPerimeter();
         Scanner scanner = new Scanner(System.in);
         int t = scanner.nextInt();
         while (t-- > 0) {
             int n = scanner.nextInt();
-            System.out.println(SIEVE[n]);
+            System.out.println(SIEVEI[n]);
         }
         scanner.close();
     }
 
-    private static void getMaxPerimeter() {
-        TRIPLET_LIST.addAll(TRIPLET_SET);
-        Collections.sort(TRIPLET_LIST);
-        for (int i = 0; i <= MAX_P; i++) {
-            SIEVE[i] = 0;
-            ISPRIME[i] = true;
-        }
-        for (int p : TRIPLET_LIST) {
-            for (int i = p * 2; i <= MAX_P; i += p) {
-                ISPRIME[i] = false;
-            }
-        }
-        for (int p : TRIPLET_LIST) {
-            if (!ISPRIME[p]) {
-                continue;
-            }
-            for (int i = p; i <= MAX_P; i += p) {
-                SIEVE[i]++;
-            }
-        }
-        int cur = 0;
-        int curi = 0;
-        for (int i = 0; i <= MAX_P; i++) {
-            if (SIEVE[i] > cur) {
-                cur = SIEVE[i];
-                curi = i;
-            }
-            SIEVE[i] = curi;
-        }
-    }
-
     private static void generateTriplets() {
-        for (int m = 3; m < 3000; m += 2) {
+
+        for (int m = 3; m < 2300; m += 2) {
             int msq = m * m;
             for (int n = 1; n <= m; n += 2) {
                 if (n != 1 && m % n == 0) {
@@ -71,13 +60,25 @@ public class Solution {
                 if (p > MAX_P) {
                     continue;
                 }
-                if (TRIPLET_SET.contains(p)) {
-                    continue;
-                }
-                TRIPLET_SET.add(p);
+                Triple triple = new Triple(a, b, c);
+                TRIPLESET.put(p, triple);
             }
         }
-    }
 
+        for (int p : TRIPLESET.keySet()) {
+            for (long i = p; i <= MAX_P; i += p) {
+                SIEVE[(int) i]++;
+            }
+        }
+        int cur = 0;
+        int curi = 0;
+        for (int i = 0; i <= MAX_P; i++) {
+            if (SIEVE[i] > cur) {
+                cur = SIEVE[i];
+                curi = i;
+            }
+            SIEVEI[i] = curi;
+        }
+    }
 }
 
