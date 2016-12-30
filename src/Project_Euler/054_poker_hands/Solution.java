866c003f6fdb9720a35ac6d49db1668fb1a79ee1
Hwanho Yong <hwanho.yong@samsung.com>
2016-12-30 13:14:30 -0500

    Project Euler #54

--- src/Project_Euler/054_poker_hands/Solution.java
+++ src/Project_Euler/054_poker_hands/Solution.java
@@ -1,215 +1,117 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Solution {
 
-    public enum POWER {
-        NOTHING(0), HIGH_CARD(1), ONE_PAIR(2), TWO_PAIRS(3), THREE_OF_A_KIND(4), STRAIGHT(5), FLUSH(6), FULL_HOUSE(7), FOUR_OF_A_KIND(8), STRAIGHT_FLUSH(
-                9), ROYAL_FLUSH(10);
+    public enum LEVEL {
+        TWO(1), THREE(2), FOUR(3), FIVE(4), SIX(5), SEVEN(6), EIGHT(7), NINE(8), TEN(9), JACK(10), QUEEN(11), KING(12), ACE(13);
 
-        public final int level;
+        private final int level;
 
-        POWER(int level) {
+        private LEVEL(int level) {
             this.level = level;
         }
     }
 
-    public enum Type {
-        SPADE(4), DIAMOND(3), HEART(2), CLOVER(1);
-        public final int level;
+    public enum TYPE {
+        CLOVER(1), HEART(2), DIAMOND(3), SPADE(4);
 
-        Type(int level) {
+        private final int level;
+
+        private TYPE(int level) {
             this.level = level;
         }
     }
 
     private static class Card {
-        public int mNumber;
-        public Type mType;
 
-        public Card(String card) {
-            char c1 = card.charAt(0);
-            switch (c1) {
-                case '1':
-                    mNumber = 1;
-                    break;
-                case '2':
-                    mNumber = 2;
-                    break;
-                case '3':
-                    mNumber = 3;
-                    break;
-                case '4':
-                    mNumber = 4;
-                    break;
-                case '5':
-                    mNumber = 5;
-                    break;
-                case '6':
-                    mNumber = 6;
-                    break;
-                case '7':
-                    mNumber = 7;
-                    break;
-                case '8':
-                    mNumber = 8;
-                    break;
-                case '9':
-                    mNumber = 9;
-                    break;
-                case 'J':
-                case 'j':
-                    mNumber = 10;
-                    break;
-                case 'Q':
-                case 'q':
-                    mNumber = 11;
-                    break;
-                case 'K':
-                case 'k':
-                    mNumber = 12;
-                    break;
-                case 'A':
-                case 'a':
-                    mNumber = 13;
-            }
-            char c2 = card.charAt(1);
-            switch (c2) {
-                case 'S':
-                case 's':
-                    mType = Type.SPADE;
-                    break;
-                case 'D':
-                case 'd':
-                    mType = Type.DIAMOND;
-                    break;
-                case 'H':
-                case 'h':
-                    mType = Type.HEART;
-                    break;
-                case 'C':
-                case 'c':
-                    mType = Type.CLOVER;
-                    break;
-            }
+        public LEVEL mLevel;
+        public TYPE mType;
+
+        public Card(LEVEL level, TYPE type) {
+            mLevel = level;
+            mType = type;
         }
     }
 
-    private static class InHand implements Comparable<InHand> {
+    private static class Player {
 
-        private ArrayList<Card> mCards;
-        private POWER mPower;
-        private POWER mSecond;
-        private Type mRepresentType;
+        private ArrayList<Card> mInHands = new ArrayList<Card>();
 
-        private boolean isRoyal = false;
-        private boolean isFlush = false;
-        private boolean isStraight = false;
-        private boolean isFourOfAkind = false;
-        private boolean isThreeOfAKind = false;
-        private boolean isTwoPairs = false;
-        private boolean isOnePair = false;
+        private boolean mIsFlush = false;
+        private boolean mIsRoyal = false;
 
-        public InHand(String c1, String c2, String c3, String c4, String c5) {
-            Card card1 = new Card(c1);
-            Card card2 = new Card(c2);
-            Card card3 = new Card(c3);
-            Card card4 = new Card(c4);
-            Card card5 = new Card(c5);
-            mCards.add(card1);
-            mCards.add(card2);
-            mCards.add(card3);
-            mCards.add(card4);
-            mCards.add(card5);
-            Collections.sort(mCards);
-            if (checkRoyalFlush()) {
-                return;
-            }
-            if (checkStraightFlush()) {
-                return;
-            }
-            if (checkFourOfAKind()) {
-                return;
-            }
-            if (checkFlush()) {
-                return;
-            }
-            if (checkStraight()) {
-                return;
-            }
-            if (checkThreeOfAKind()) {
-                return;
-            }
-            if (checkTwoPairs()) {
-                return;
-            }
-            if (checkOnePair()) {
-                return;
-            }
+        public void addCard(Card card) {
+            mInHands.add(card);
         }
 
-        private boolean checkOnePair() {
-            // TODO Auto-generated method stub
-            return false;
-        }
-
-        private boolean checkTwoPairs() {
-            // TODO Auto-generated method stub
-            return false;
-        }
-
-        private boolean checkThreeOfAKind() {
-            // TODO Auto-generated method stub
-            return false;
-        }
-
-        private boolean checkStraight() {
-            // TODO Auto-generated method stub
-            return false;
-        }
-
-        private boolean checkFlush() {
-            // TODO Auto-generated method stub
-            return false;
-        }
-
-        private boolean checkFourOfAKind() {
-            // TODO Auto-generated method stubm
-            return false;
-        }
-
-        private boolean checkStraightFlush() {
-            // TODO Auto-generated method stub
-            return false;
-        }
-
-        private boolean checkRoyalFlush() {
-            Card firstCard = mCards.get(0);
-            Type type = mCards.get(0)
-            for(int i=1;i<5;i++) {
-                
-                
+        /**
+         * Borrowed idea from Nayuki.io
+         * https://github.com/nayuki/Project-Euler-solutions/blob/master/java/p054.java
+         */
+        public int checkPower() {
+            int[] countByCard = new int[13];
+            mIsFlush = true;
+            TYPE flushCheck = mInHands.get(0).mType;
+            for (Card card : mInHands) {
+                countByCard[card.mLevel.level - 1]++;
+                if (card.mType != flushCheck) {
+                    mIsFlush = false;
+                }
             }
-            
-            
-            this.mPower = POWER.ROYAL_FLUSH;
-            return true;
+            int[] countByLevel = new int[6];
+            for (int count : countByCard) {
+                countByLevel[count]++;
+            }
+            int bestCards = get5FrequentHighestCards(countByCard, countByLevel);
+            int straightHighRank = getStraightHighRank(countByCard);
+            mIsRoyal = (countByCard[12] != 0) && (countByCard[11] != 0) && (countByCard[10] != 0) && (countByCard[9] != 0) && (countByCard[8] != 0);
+
+            if (mIsFlush && mIsRoyal)
+                return 9 << 20 | bestCards; // Royal flush
+            else if (straightHighRank != -1 && mIsFlush)
+                return 8 << 20 | straightHighRank; // Straight flush
+            else if (countByLevel[4] == 1)
+                return 7 << 20 | bestCards; // Four of a kind
+            else if (countByLevel[3] == 1 && countByLevel[2] == 1)
+                return 6 << 20 | bestCards; // Full house
+            else if (mIsFlush)
+                return 5 << 20 | bestCards; // Flush
+            else if (straightHighRank != -1)
+                return 4 << 20 | straightHighRank; // Straight
+            else if (countByLevel[3] == 1)
+                return 3 << 20 | bestCards; // Three of a kind
+            else if (countByLevel[2] == 2)
+                return 2 << 20 | bestCards; // Two pairs
+            else if (countByLevel[2] == 1)
+                return 1 << 20 | bestCards; // One pair
+            else
+                return 0 << 20 | bestCards;
         }
 
-        @Override
-        public int compareTo(InHand target) {
-            if (this.mPower.level < target.mPower.level) {
-                return -1;
-            } else if (this.mPower.level > target.mPower.level) {
-                return 1;
+        private static int get5FrequentHighestCards(int[] ranks, int[] ranksHist) {
+            int result = 0;
+            int count = 0;
+            for (int i = ranksHist.length - 1; i >= 0; i--) {
+                for (int j = ranks.length - 1; j >= 0; j--) {
+                    if (ranks[j] == i) {
+                        for (int k = 0; k < i && count < 5; k++, count++)
+                            result = result << 4 | j;
+                    }
+                }
             }
-            if (this.mSecond.level < target.mSecond.level) {
-                return -1;
-            } else if (this.mSecond.level > target.mSecond.level) {
-                return 1;
+            return result;
+        }
+
+        private static int getStraightHighRank(int[] ranks) {
+            outer: for (int i = ranks.length - 1; i >= 3; i--) {
+                for (int j = 0; j < 5; j++) {
+                    if (ranks[(i - j + 13) % 13] == 0)
+                        continue outer; // Current offset is not a straight
+                }
+                return i; // Straight found
             }
-            return this.mRepresentType.level < target.mRepresentType.level ? -1 : 1;
+            return -1;
         }
     }
 
@@ -217,19 +119,23 @@ public class Solution {
         Scanner scanner = new Scanner(System.in);
         int T = scanner.nextInt();
         while (T-- > 0) {
-            String s1 = scanner.next();
-            String s2 = scanner.next();
-            String s3 = scanner.next();
-            String s4 = scanner.next();
-            String s5 = scanner.next();
-            InHand ih1 = new InHand(s1, s2, s3, s4, s5);
-            s1 = scanner.next();
-            s2 = scanner.next();
-            s3 = scanner.next();
-            s4 = scanner.next();
-            s5 = scanner.next();
-            InHand ih2 = new InHand(s1, s2, s3, s4, s5);
-            if (ih1.compareTo(ih2) < 0) {
+            Player p1 = new Player();
+            for (int i = 0; i < 5; i++) {
+                String input = scanner.next();
+                TYPE type = getType(input);
+                LEVEL level = getLevel(input);
+                Card card = new Card(level, type);
+                p1.addCard(card);
+            }
+            Player p2 = new Player();
+            for (int i = 0; i < 5; i++) {
+                String input = scanner.next();
+                TYPE type = getType(input);
+                LEVEL level = getLevel(input);
+                Card card = new Card(level, type);
+                p2.addCard(card);
+            }
+            if (p1.checkPower() < p2.checkPower()) {
                 System.out.println("Player 2");
             } else {
                 System.out.println("Player 1");
@@ -238,5 +144,82 @@ public class Solution {
         scanner.close();
     }
 
+    private static TYPE getType(String input) {
+        TYPE type;
+        char c = input.charAt(1);
+        switch (c) {
+            case 'S':
+            case 's':
+                type = TYPE.SPADE;
+                break;
+            case 'D':
+            case 'd':
+                type = TYPE.DIAMOND;
+                break;
+            case 'H':
+            case 'h':
+                type = TYPE.HEART;
+                break;
+            case 'C':
+            case 'c':
+            default:
+                type = TYPE.CLOVER;
+                break;
+        }
+        return type;
+    }
+
+    private static LEVEL getLevel(String input) {
+        LEVEL level;
+        char lc = input.charAt(0);
+        switch (lc) {
+            case '2':
+                level = LEVEL.TWO;
+                break;
+            case '3':
+                level = LEVEL.THREE;
+                break;
+            case '4':
+                level = LEVEL.FOUR;
+                break;
+            case '5':
+                level = LEVEL.FIVE;
+                break;
+            case '6':
+                level = LEVEL.SIX;
+                break;
+            case '7':
+                level = LEVEL.SEVEN;
+                break;
+            case '8':
+                level = LEVEL.EIGHT;
+                break;
+            case '9':
+                level = LEVEL.NINE;
+                break;
+            case 'T':
+            case 't':
+                level = LEVEL.TEN;
+                break;
+            case 'J':
+            case 'j':
+                level = LEVEL.JACK;
+                break;
+            case 'Q':
+            case 'q':
+                level = LEVEL.QUEEN;
+                break;
+            case 'K':
+            case 'k':
+                level = LEVEL.KING;
+                break;
+            case 'A':
+            case 'a':
+            default:
+                level = LEVEL.ACE;
+                break;
+        }
+        return level;
+    }
 }
 
